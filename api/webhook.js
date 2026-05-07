754f39b557f55296e9caccb56afa82e9567cf0b8
yeversoncano-art <yeversoncano@gmail.com>
2026-05-06 22:24:23 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
 
