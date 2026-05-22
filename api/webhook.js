c83a4f40868b60662ffad9599242e88b6c179dc9
yeversoncano-art <yeversoncano@gmail.com>
2026-05-22 12:49:11 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -333,7 +333,10 @@ for (const item of node.media) {
 
     await new Promise(r => setTimeout(r, 700));
   }
-     const response = await fetch(
+
+}
+
+const response = await fetch(
   `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
   {
     method: "POST",
