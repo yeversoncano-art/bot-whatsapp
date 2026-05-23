c367b844dfbe42552b4b829041217868a0243687
yeversoncano-art <yeversoncano@gmail.com>
2026-05-22 22:19:32 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -287,7 +287,10 @@ if (Array.isArray(node?.media)) {
 
 for (const item of node.media) {
 
-    if (item.type === "text") {
+    if (
+  item.type === "text" &&
+  buttons.length === 0
+) {
 
       await fetch(
         `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
